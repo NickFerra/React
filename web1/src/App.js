f96e235987ee8ffb212cb0e4a9ef1dd21a2eba6e
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-16 12:03:25 +0000

    Adding top row of images and text

--- web1/src/App.js
+++ web1/src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Article from "./components/Article.js";
 import A from "./A.jpg";
 import B from "./B.jpg";
+import C from "./C.jpg";
+import D from "./D.jpg";
+import E from "./E.jpg";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <div className="boxHold">
         <Article alph="A" image={A} />
         <Article alph="B" image={B} />
+        <Article alph="C" image={C} />
+        <Article alph="D" image={D} />
+        <Article alph="E" image={E} />
       </div>
     </div>
   );
