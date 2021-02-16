0d82a527c46fad44fc33c57d6c8d8c4b8a2d8a5a
Nicholas Ferrari <Nick.ferrari@me.com>
2021-02-16 11:38:04 +0000

    formatting

--- web1/src/App.js
+++ web1/src/App.js
@@ -1,20 +1,17 @@
-import './App.css';
-import Article from './components/Article.js';
-import A from './A.jpg';
-import B from './B.jpg';
+import "./App.css";
+import Article from "./components/Article.js";
+import A from "./A.jpg";
+import B from "./B.jpg";
 
 function App() {
   return (
     <div>
-      <div className = "boxHold">
-        <Article alph = "A" image = {A}/>
-        <Article alph = "B" image = {B}/> 
+      <div className="boxHold">
+        <Article alph="A" image={A} />
+        <Article alph="B" image={B} />
       </div>
     </div>
   );
 }
-
-
-
 
 export default App;
--- web1/src/components/Article.js
+++ web1/src/components/Article.js
@@ -1,19 +1,11 @@
+const Article = (props) => {
+  return (
+    <div className="imageText">
+      <img src={props.image} className="letters" alt="letter image" />
 
-
-
-
-const Article = (props) =>{
-    return(
-      
-      
-      <div className = "imageText">
-  
-        <img src = {props.image} className ="letters" alt = "letter image"/>
-        
-        <p>Here is letter {props.alph}</p> 
-        
-      </div>
-    );
-}
+      <p>Here is letter {props.alph}</p>
+    </div>
+  );
+};
 
 export default Article;
